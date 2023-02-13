c0f410d93663a9cc2ba3167c62e50db8680cfec8
User anna_kaname <nastyapiar@gmail.com>
2023-02-13 16:00:39 +0500

    правки в классе

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import time
 
 
 class BinanceCheckCurrencies:
-    apikey = 'vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A'
-    secret = 'NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j'
-    client = Client(apikey, secret)
-    currencies = 'XRPUSDT'
-    time_check = '60'
-    start_str = time_check + ' minutes ago UTC'
     price_list = []
     res_price_list = []
     max_price_index = None
+
+    def __init__(self, currencies, time_check):
+        self.apikey = 'vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A'
+        self.secret = 'NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j'
+        self.client = Client(self.apikey, self.secret)
+        self.currencies = currencies
+        self.time_check = time_check
+        self.start_str = self.time_check + ' minutes ago UTC'
 
     @staticmethod
     def search_difference(max_num, min_num, percent_difference):
@@ -32,25 +34,16 @@ class BinanceCheckCurrencies:
             del self.res_price_list[0:self.max_price_index]
             res = self.search_difference(max(self.res_price_list), min(self.res_price_list), 1)
             if res:
-                print('max_price_index ->', self.max_price_index,
-                      'Была максимальная цена за час', max(self.res_price_list))
+                return print(f'За час цена упала больше 1%. '
+                             f'Было - {max(self.res_price_list)}, '
+                             f'стало - {min(self.res_price_list)}')
 
-                return print(f'Была максимальная цена за час {max(self.res_price_list)}. '
-                             f'Минимальная цена за час {min(self.res_price_list)}')
-
-                # return print(f'За час цена упала больше 1%. Было {max(self.res_price_list)}')
-
-
-sheck = BinanceCheckCurrencies()
 
 while True:
-    try:
-        sheck.main()
-        # print(sheck.price_list)
-        # print(sheck.res_price_list)
-        time.sleep(3600)
-    except Exception as err:
-        print(err)
 
-# print(sheck.price_list)
-# print(sheck.res_price_list)
+    check_price = BinanceCheckCurrencies('XRPUSDT', '60')
+    check_price.main()
+    # print(check_price.price_list)
+    # print(check_price.res_price_list)
+    time.sleep(100)
+
