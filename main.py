1afdefa2eca815344000ac1a13d5d14cfa884bb2
User anna_kaname <nastyapiar@gmail.com>
2023-02-14 11:11:33 +0500

    Изменен код и логика задачи. Для удобства добавлены коммиты к каждой функции

--- main.py
+++ main.py
@@ -1,49 +1,105 @@
-from binance import Client
+import json
 import time
 
+from binance import Client
+import requests
+import pandas as pd
 
-class BinanceCheckCurrencies:
-    price_list = []
-    res_price_list = []
-    max_price_index = None
+from datetime import datetime
 
-    def __init__(self, currencies, time_check):
-        self.apikey = 'vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A'
-        self.secret = 'NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j'
-        self.client = Client(self.apikey, self.secret)
-        self.currencies = currencies
-        self.time_check = time_check
-        self.start_str = self.time_check + ' minutes ago UTC'
+# Коды не привязаны к личному кабинету в binance. Взяты из документации с апи.
+# При работе со своим личным кабинетом, нужно вставить свои, но лучше их "спрятать" через denv.
+# при данном коде данные берутся из апи напрямую, поэтому они по сути тут и не нужны.
+# Только для теста следующих операция оставляю их тут.
 
-    @staticmethod
-    def search_difference(max_num, min_num, percent_difference):
-        difference = max_num / 100 * percent_difference
-        if max_num - min_num >= difference:
-            return True
+apikey = 'vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A'
+secret = 'NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j'
+client = Client(apikey, secret)
 
-    def main(self):
-        agg_trades = self.client.aggregate_trade_iter(symbol=self.currencies, start_str=self.start_str)
 
-        for trade in agg_trades:
-            if float(trade['p']) not in self.price_list:
-                self.price_list.append(float(trade['p']))
+# основной код
 
-            self.max_price_index = self.price_list.index(max(self.price_list))
+def get_data():
+    """
+    Обработка данных нужной нам валюты напрямую через ссылку апи.
+    Возвращает значение список с float(ценой) и дату (дата для проверки кода)
+    """
 
-            self.res_price_list = self.price_list
-            del self.res_price_list[0:self.max_price_index]
-            res = self.search_difference(max(self.res_price_list), min(self.res_price_list), 1)
-            if res:
-                return print(f'За час цена упала больше 1%. '
-                             f'Было - {max(self.res_price_list)}, '
-                             f'стало - {min(self.res_price_list)}')
+    link_api = requests.get('https://api3.binance.com/api/v3/ticker/price')
+    price = link_api.json()[306]
+    times = datetime.now().strftime("%H:%M:%S")
+    data = [float(price['price']), times]
+    return data
+
+
+price_data = []
+
+
+def write_data():
+    """
+    Запись полученных данных в список и перевод в json
+    Стоит таймер, чтобы брать каждую секунду данные. Стоит 0.5 так как пинг от сервера еще ~ 0.5 сек
+    Каждый раз идет дозапись в список price_data
+    Если список стал слишком большой, то очищаю значения от 0 до 7200
+    """
+    if len(price_data) >= 10800:
+        del price_data[0:7200]
+
+    for _ in range(10):
+        time.sleep(0.5)
+        price_data.append(get_data())
+    res_json = json.dumps(price_data)
+
+    return res_json
+
+
+def get_max_min(some_data):
+    """
+    Поиск максимальной/минимальной цены и разницы между ними
+    """
+
+    max_price = max(some_data[0])
+    min_price = min(some_data[0])
+    difference = max_price / 100 * 1
+
+    return max_price, min_price, difference
+
+
+def get_max():
+    """
+    Читаем json через pandas. Так как он позволяет лучше работать с огромным потоком данным.
+    И удобно проверять только последние 3600 записей (столько записей будет в течение часа)
+    - для отслеживания цены в течение часа.
+    """
+
+    stops = pd.read_json(write_data())
+    if len(price_data) > 3600:
+        last_line = stops.tail(3600)
+        print('Проверяю последние строчки:\n', last_line)
+        max_price = get_max_min(last_line)[0]
+        min_price = get_max_min(last_line)[1]
+        difference = get_max_min(last_line)[2]
+        print('                         max_price', max_price)  # максимальное значение в столбце
+        print('                         min_price', min_price)  # максимальное значение в столбце
+        print(difference)
+        if max_price - min_price >= difference:
+            print('Внимание! Цена стала больше 1%')
+    else:
+        print('Проверка начальных')
+        max_price = get_max_min(stops)[0]
+        min_price = get_max_min(stops)[1]
+        difference = get_max_min(stops)[2]
+        print('                         max_price', max_price)  # максимальное значение в столбце
+        print('                         min_price', min_price)  # максимальное значение в столбце
+        print(difference)
+        if max_price - min_price >= difference:
+            print('Внимание! Цена стала больше 1%')
+
+
+def main():
+    get_max()
 
 
 while True:
-
-    check_price = BinanceCheckCurrencies('XRPUSDT', '60')
-    check_price.main()
-    # print(check_price.price_list)
-    # print(check_price.res_price_list)
-    time.sleep(100)
-
+    if __name__ in '__main__':
+        main()
